ea7cb953c73d792fb0fc0f491807377070e2d4ba
RakshithNM <rakshith_bellare@outlook.com>
2017-08-31 23:28:49 +0530

    Removed irrelevant files and updated others

--- src/index.js
+++ src/index.js
@@ -1,79 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-class Switch extends React.Component {
-  render() {
-    return (
-      <button className="switch">b</button>
-    );
-  }
-}
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}></button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      squares: Array(100).fill(null),
-    };
-  } 
-
-  renderSquare(i) {
-    return <Square key = {i} value={this.state.squares[i]} />;    
-  }
-
-  /*invokeGeneration() {
-    
-  }*/
-
-  render() {
-    var keyArrayLength = 10; 
-    var keyArray = [...Array(keyArrayLength).keys()];
-
-    return (
-      <div>
-        {
-          keyArray.map ( (n) => {
-            return <div key = {n} className="board-row">
-              {
-                keyArray.map ( (n) => {
-                  return this.renderSquare(n)
-                })
-              }
-            </div>
-          })
-        }
-        <Switch onClick={() => this.invokeGeneration()} />
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+import App from './App';
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+	<App key={1993} numberOfContinents={7}/>,
+	document.getElementById('root')
 );
